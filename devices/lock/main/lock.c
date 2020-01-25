59b9ae41df802e87f46b03f6c831e791a8022404
Simon Werner9999 <simonwerner@gmail.com>
2020-01-25 19:06:52 +1300

    Move device code here

---
+++ devices/lock/main/lock.c
@@ -0,0 +1,126 @@
+#include <stdint.h>
+#include <stddef.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+#include "common.h"
+#include "lock.h"
+
+#define DEVICE_TYPE ("lock")
+
+static const char *TAG = "LOCK";
+static TaskHandle_t unlock_task_handle = NULL;
+
+//
+// Globals
+// TODO: Put into a struct and pass it around
+//
+char topic_register[MAX_TOPIC_LEN];
+char topic_unlock[MAX_TOPIC_LEN];
+
+
+void device_unlock() {
+    ESP_LOGI(TAG, "UNLOCK");
+    gpio_set_level(GPIO_04, GPIO_OFF);
+}
+
+void device_lock() {
+    ESP_LOGI(TAG, "LOCK");
+    gpio_set_level(GPIO_04, GPIO_ON);
+}
+
+void v_unlock_task( void * fp_sleep_time_secs ) {
+
+    int sleep_time_secs = (int)fp_sleep_time_secs;
+
+    if (sleep_time_secs <= 0) goto CLEAN_EXIT;
+    if (sleep_time_secs >= TIME_24_HOURS_IN_SEC) goto CLEAN_EXIT;
+
+    device_unlock();
+    ESP_LOGW(TAG, "Sleeping for %d seconds", sleep_time_secs);
+    vTaskDelay((sleep_time_secs * 1000) / portTICK_PERIOD_MS);
+    device_lock();
+
+CLEAN_EXIT:
+    // You need to vTaskDelete to cleaning exit the thread
+    kill_unlock_task();
+}
+
+void kill_unlock_task() {
+    if (unlock_task_handle == NULL) {
+        ESP_LOGI(TAG, "Task is already dead");
+        return;
+    }
+
+    TaskHandle_t tmp_task_handle = unlock_task_handle;
+    unlock_task_handle = NULL;
+    vTaskDelete(tmp_task_handle);
+}
+
+void device_unlock_for_time(int sleep_time_secs) {
+    TaskHandle_t xHandle = NULL;
+
+    // Kill the task if it's already running.
+    kill_unlock_task();
+
+    // Create a task that will stop after a certain time
+    xTaskCreate( v_unlock_task, "LOCKER_CODE", 2048, (void *)sleep_time_secs, tskIDLE_PRIORITY, &xHandle );
+
+    unlock_task_handle = xHandle;
+}
+
+void lock_handle_incoming_data(esp_mqtt_event_handle_t event) {
+    if (strcmp(topic_unlock, event->topic) != 0) {
+        ESP_LOGI(TAG, "Got unlock command.  Unlocking for (%.*s) seconds", event->data_len, event->data);
+
+        #define MAX_TOPIC_DATA_LEN 6
+        if (event->data_len > MAX_TOPIC_DATA_LEN) {
+            ESP_LOGE(TAG, "Invalid data length: %d.  Rebooting", event->data_len);
+            esp_restart();
+        }
+
+        char sleep_time_secs_str[MAX_TOPIC_DATA_LEN + 1];
+
+        snprintf(sleep_time_secs_str, event->data_len + 1, "%s", event->data);
+        int sleep_time_secs = atoi(sleep_time_secs_str);
+        device_unlock_for_time(sleep_time_secs);   
+    }
+}
+
+char *lock_get_topic_register() {
+    return topic_register;
+}
+
+char *lock_get_topic_unlock() {
+    return topic_unlock;
+}
+
+const char *lock_get_device_type() {
+    return DEVICE_TYPE;
+}
+
+void lock_init() {
+    // Get the MAC address, from that we can create the topic sub/pubs
+    set_my_uuid();
+    sprintf(topic_register, BASE_TOPIC "/%s/register", get_uuid());
+    sprintf(topic_unlock,   BASE_TOPIC "/%s/unlock", get_uuid());
+
+    device_lock(); // By defaut we are locked
+
+    //configure GPIO with the given settings
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+}
+
---
+++ devices/rfid-reader/README.md
@@ -0,0 +1,12 @@
+
+This code is based on the code from Gareth Evens (https://github.com/agrath/Hackland.AccessControl).
+
+| Signal        | Pin | GPIO | Comment                                 |
+| ------------- | --- | ---- | --------------------------------------- |
+| RST/Reset     | D3  | 0    | When held high, then we reset the RC522 |
+| SPI CS/SS/SDA | D8  | 15   | The Slave Select                        |
+| SPI MOSI      | D7  | 13   | The MOSI data                           |
+| SPI MISO      | D6  | 12   | The MISO data                           |
+| SPI SCK       | D5  | 14   | The clock                               |
+
+sda,sck,mosi,miso,...,gnd,rst,3.3v
